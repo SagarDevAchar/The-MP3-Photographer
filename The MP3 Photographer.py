d818e84dc5ecb8b39aa9a1b03db7ba9c02a12ac0
Sagar Dev Achar <51400137+SagarDevAchar@users.noreply.github.com>
2022-08-11 01:45:32 +0530

    Add files via upload

--- The MP3 Photographer.py
+++ The MP3 Photographer.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from tqdm import trange
-from os import listdir
+from os import listdir, system
 from time import sleep
 
 
@@ -35,51 +35,55 @@ LOGO = """
                                                  ▀▀   ▀    ▀▀▀ ▀ ▀ ▀▀▀ ▀ ▀ ▀ ▀   ▀▀  ▀▀▀  ▀    ▀ ▀ ▀▀▀ ▀ ▀ ▀ ▀ ▀ ▀
 """ + SECTION_SEPERATOR
 
-print(LOGO)
+while True:
+    print(LOGO)
 
-print("INSTRUCTIONS\n")
-print("1.\tPlace the JPG files with '<filename>.jpg' in the adjacent directory labeled 'IN'")
-print("2.\tPlace the MP3 files with '<filename>.mp3' in the adjacent directory labeled 'IN'")
-print("3.\tWell, that's it, run this code")
+    print("INSTRUCTIONS\n")
+    print("1.\tPlace the JPG / JPEG / PNG files as '<filename>.jpg/jpeg/png' in the adjacent directory labeled 'IN'")
+    print("2.\tPlace the MP3 files as '<filename>.mp3' in the adjacent directory labeled 'IN'")
+    print("3.\tWell, that's it, run this code")
 
-print("\n<filename.png> will be available in the adjacent directory labeled 'SRC'")
+    print("\n<filename.png> will be available in the adjacent directory labeled 'SRC'")
 
-print(SECTION_SEPERATOR)
+    print(SECTION_SEPERATOR)
 
-input('Press ENTER to start the conversion process...')
+    input('Press ENTER to start the conversion process...')
 
-W, H = 1280, 720
+    W, H = 1280, 720
 
-SONG_NAMES = []
-for filename in listdir('IN'):
-    filename = filename.split('.')
-    if filename[1] == 'jpg':
-        SONG_NAMES.append(filename[0])
-
-print('\nJPG Files Found = %d' % len(SONG_NAMES))
-
-print(SECTION_SEPERATOR)
-
-for SONG_NAME in SONG_NAMES:
     try:
-        IMAGE = cv2.resize(cv2.imread("IN/%s.jpg" % SONG_NAME, cv2.IMREAD_COLOR), (W, H))
-        AUDIO = open("IN/%s.mp3" % SONG_NAME, 'rb').read()
+        SONG_NAMES = []
+        for filename in listdir('IN'):
+            filename = filename.split('.')
+            if filename[1] == 'jpg' or filename[1] == 'jpeg' or filename[1] == 'png':
+                SONG_NAMES.append((filename[0], filename[1]))
 
-        print(f"\nWriting {SONG_NAME:s}.mp3 into of {SONG_NAME:s}.png")
+        print('\nJPG Files Found = %d' % len(SONG_NAMES))
 
-        assert len(AUDIO) <= W * H * 3 / 4
-        sleep(0.1)
+        print(SECTION_SEPERATOR)
 
-        STEGANO_ERASED_IMAGE = IMAGE & 0b11111100
-        STEGANO_OR = get_stegano_or(AUDIO)
-        STEGANO_WRITTEN_IMAGE = STEGANO_ERASED_IMAGE | STEGANO_OR
+        for SONG_NAME, IMAGE_FORMAT in SONG_NAMES:
+            try:
+                IMAGE = cv2.resize(cv2.imread(f"IN/{SONG_NAME}.{IMAGE_FORMAT}", cv2.IMREAD_COLOR), (W, H))
+                AUDIO = open("IN/%s.mp3" % SONG_NAME, 'rb').read()
 
-        cv2.imwrite(f'SRC/{SONG_NAME:s}.png', STEGANO_WRITTEN_IMAGE)
-    except AssertionError:
-        print("ERROR: Cannot create %s.png! MP3 is too big for the JPG" % SONG_NAME)
-    except Exception as e:
-        print("ERROR:   Failed to create %s.png! Encountered the following error: %s" % (SONG_NAME, str(e)))
+                print(f"\nWriting {SONG_NAME:s}.mp3 into of {SONG_NAME:s}.png")
 
-print(SECTION_SEPERATOR)
+                assert len(AUDIO) <= W * H * 3 / 4
+                sleep(0.1)
 
-input('Press ENTER to terminate...')
+                STEGANO_ERASED_IMAGE = IMAGE & 0b11111100
+                STEGANO_OR = get_stegano_or(AUDIO)
+                STEGANO_WRITTEN_IMAGE = STEGANO_ERASED_IMAGE | STEGANO_OR
+
+                cv2.imwrite(f'SRC/{SONG_NAME:s}.png', STEGANO_WRITTEN_IMAGE)
+            except AssertionError:
+                print("ERROR: Cannot create %s.png! MP3 is too big for the JPG" % SONG_NAME)
+            except Exception as e:
+                print("ERROR:   Failed to create %s.png! Encountered the following error: %s" % (SONG_NAME, str(e)))
+    except FileNotFoundError:
+        print("Looks like you have lost the 'IN' folder!\nUnfortunately, I cannot work without those...")
+
+    print(SECTION_SEPERATOR)
+    input("Press ENTER to start over")
+    system('cls')
